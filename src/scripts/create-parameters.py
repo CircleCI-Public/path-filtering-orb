6f77f22eb3a7eaebad5e28ed4a8eec3615d79368
Michael Burt <michael.burt@Michaels-MacBook-Pro.local>
2021-05-18 16:56:37 -0600

    formatting

--- src/scripts/create-parameters.py
+++ src/scripts/create-parameters.py
@@ -5,78 +5,64 @@ import os
 import re
 import subprocess
 
-output_path = os.environ.get("OUTPUT_PATH")
-head = os.environ.get("CIRCLE_SHA1")
-base = (
-    subprocess.run(
-        ["git", "merge-base", os.environ.get("BASE_REVISION"), head],
-        check=True,
-        capture_output=True,
-    )
-    .stdout.decode("utf-8")
-    .strip()
-)
+output_path = os.environ.get('OUTPUT_PATH')
+head = os.environ.get('CIRCLE_SHA1')
+base = subprocess.run(
+  ['git', 'merge-base', os.environ.get('BASE_REVISION'), head],
+  check=True,
+  capture_output=True
+).stdout.decode('utf-8').strip()
 
 if head == base:
-    try:
-        # If building on the same branch as BASE_REVISION, we will get the
-        # current commit as merge base. In that case try to go back to the
-        # first parent, i.e. the last state of this branch before the
-        # merge, and use that as the base.
-        base = (
-            subprocess.run(
-                [
-                    "git",
-                    "rev-parse",
-                    "HEAD~1",
-                ],  # FIXME this breaks on the first commit, fallback to something
-                check=True,
-                capture_output=True,
-            )
-            .stdout.decode("utf-8")
-            .strip()
-        )
-    except:
-        # This can fail if this is the first commit of the repo, so that
-        # HEAD~1 actually doesn't resolve. In this case we can compare
-        # against this magic SHA below, which is the empty tree. The diff
-        # to that is just the first commit as patch.
-        base = "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
+  try:
+    # If building on the same branch as BASE_REVISION, we will get the
+    # current commit as merge base. In that case try to go back to the
+    # first parent, i.e. the last state of this branch before the
+    # merge, and use that as the base.
+    base = subprocess.run(
+      ['git', 'rev-parse', 'HEAD~1'], # FIXME this breaks on the first commit, fallback to something
+      check=True,
+      capture_output=True
+    ).stdout.decode('utf-8').strip()
+  except:
+    # This can fail if this is the first commit of the repo, so that
+    # HEAD~1 actually doesn't resolve. In this case we can compare
+    # against this magic SHA below, which is the empty tree. The diff
+    # to that is just the first commit as patch.
+    base = '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
 
-print("Comparing {}...{}".format(base, head))
-changes = (
-    subprocess.run(
-        ["git", "diff", "--name-only", base, head], check=True, capture_output=True
-    )
-    .stdout.decode("utf-8")
-    .splitlines()
-)
+print('Comparing {}...{}'.format(base, head))
+changes = subprocess.run(
+  ['git', 'diff', '--name-only', base, head],
+  check=True,
+  capture_output=True
+).stdout.decode('utf-8').splitlines()
 
-mappings = [m.split() for m in os.environ.get("MAPPING").splitlines()]
+mappings = [
+  m.split() for m in
+  os.environ.get('MAPPING').splitlines()
+]
 
 
 def check_mapping(m):
-    if 3 != len(m):
-        raise Exception("Invalid mapping")
-    if m[1] == 'run-tag':
-        return True
-    path, param, value = m
-    regex = re.compile(r"^" + path + r"$")
-    for change in changes:
-        if regex.match(change):
-            return True
-    return False
+  if 3 != len(m):
+    raise Exception("Invalid mapping")
+  if m[1] == 'run-tag':
+    return True
+  path, param, value = m
+  regex = re.compile(r'^' + path + r'$')
+  for change in changes:
+    if regex.match(change):
+      return True
+  return False
 
 
 def convert_mapping(m):
-    return [m[1], json.loads(m[2])]
-
+  return [m[1], json.loads(m[2])]
 
 mappings = filter(check_mapping, mappings)
 mappings = map(convert_mapping, mappings)
 mappings = dict(mappings)
 
-print(mappings)
-
-with open(output_path, "w") as fp:
-    fp.write(json.dumps(mappings))
+with open(output_path, 'w') as fp:
+  fp.write(json.dumps(mappings))
