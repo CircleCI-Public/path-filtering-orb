9dc4426b04526f8419e2333732aa06993e552809
David Alberto Montaño Fetecua <david.montano@circleci.com>
2024-08-13 22:58:06 -0500

    fix: replacing script param vars with environment variables

--- src/scripts/checkout-shallow.sh
+++ src/scripts/checkout-shallow.sh
@@ -15,17 +15,17 @@ git --version
 mkdir -p "$SSH_CONFIG_DIR"
 chmod 0700 "$SSH_CONFIG_DIR"
 
-if [ -x "$(command -v ssh-keyscan)" ] && ([ "<< parameters.keyscan_github >>" == "true" ] || [ "<< parameters.keyscan_bitbucket >>" == "true" ]); then
-    if [ "<< parameters.keyscan_github >>" == "true" ]; then
+if [ -x "$(command -v ssh-keyscan)" ] && ([ "${KEYSCAN_GITHUB}" == "true" ] || [ "${KEYSCAN_BITBUCKET}" == "true" ]); then
+    if [ "${KEYSCAN_GITHUB}" == "true" ]; then
     ssh-keyscan -H github.com >> "$SSH_CONFIG_DIR/known_hosts"
     fi
-    if [ "<< parameters.keyscan_bitbucket >>" == "true" ]; then
+    if [ "${KEYSCAN_BITBUCKET}" == "true" ]; then
     ssh-keyscan -H bitbucket.org >> "$SSH_CONFIG_DIR/known_hosts"
     fi
 fi
 
 # see: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints
-if [ "<< parameters.keyscan_github >>" != "true" ]; then
+if [ "${KEYSCAN_GITHUB}" != "true" ]; then
     echo 'github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
     github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
     github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=
@@ -33,7 +33,7 @@ if [ "<< parameters.keyscan_github >>" != "true" ]; then
 fi
 
 # see: https://bitbucket.org/blog/ssh-host-key-changes
-if [ "<< parameters.keyscan_bitbucket >>" != "true" ]; then
+if [ "${KEYSCAN_BITBUCKET}" != "true" ]; then
     echo 'bitbucket.org ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPIQmuzMBuKdWeF4+a2sjSSpBK0iqitSQ+5BM9KhpexuGt20JpTVM7u5BDZngncgrqDMbWdxMWWOGtZ9UgbqgZE=
     bitbucket.org ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIazEu89wgQZ4bqs3d63QSMzYVa0MuJ2e2gKTKqu+UUO
     bitbucket.org ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDQeJzhupRu0u0cdegZIa8e86EG2qOCsIsD1Xw0xSeiPDlCr7kq97NLmMbpKTX6Esc30NuoqEEHCuc7yWtwp8dI76EEEB1VqY9QJq6vk+aySyboD5QF61I/1WeTwu+deCbgKMGbUijeXhtfbxSxm6JwGrXrhBdofTsbKRUsrN1WoNgUa8uqN1Vx6WAJw1JHPhglEGGHea6QICwJOAr/6mrui/oB7pkaWKHj3z7d1IC4KWLtY47elvjbaTlkN04Kc/5LFEirorGYVbt15kAUlqGM65pk6ZBxtaO3+30LVlORZkxOh+LKL/BvbZ/iRNhItLqNyieoQj/uh/7Iv4uyH/cV/0b4WDSd3DptigWq84lJubb9t/DnZlrJazxyDCulTmKdOR7vs9gMTo+uoIrPSb8ScTtvw65+odKAlBj59dhnVp9zd7QUojOpXlL62Aw56U4oO+FALuevvMjiWeavKhJqlR7i5n9srYcrNV7ttmDw7kf/97P5zauIhxcjX+xHv4M=
@@ -65,7 +65,7 @@ if [ -n "$CIRCLE_TAG" ]; then
     clone_tag_args=
     fetch_tag_args="--tags"
 fi
-if [ '<< parameters.no_tags >>' == 'true' ]; then
+if [ "${NO_TAGS}" == 'true' ]; then
     clone_tag_args="--no-tags"
     fetch_tag_args="--no-tags"
 fi
@@ -78,28 +78,28 @@ else
 fi
 
 # Checkout. SourceCaching? or not.
-if [ -e "$working_directory/<< parameters.path >>/.git" ]; then
+if [ -e "$working_directory/${REPO_PATH}/.git" ]; then
     echo 'Fetching into existing repository'
     existing_repo='true'
-    cd "$working_directory/<< parameters.path >>"
+    cd "$working_directory/${REPO_PATH}"
     git remote set-url origin "$CIRCLE_REPOSITORY_URL" || true
 else
     echo 'Cloning git repository'
     existing_repo='false'
-    mkdir -p "$working_directory/<< parameters.path >>"
-    cd "$working_directory/<< parameters.path >>"
-    git clone ${clone_tag_args} --depth << parameters.depth >> $CIRCLE_REPOSITORY_URL .
+    mkdir -p "$working_directory/${REPO_PATH}"
+    cd "$working_directory/${REPO_PATH}"
+    git clone ${clone_tag_args} --depth ${DEPTH} $CIRCLE_REPOSITORY_URL .
 fi
 
 # NOTE: Original checkout fetch only if SourceCaching, but we fetch always for depth selection.
 echo 'Fetching from remote repository'
 if [ -n "$CIRCLE_TAG" ]; then
-    git fetch ${fetch_tag_args} --depth << parameters.fetch_depth >> --force --tags origin "+refs/tags/${CIRCLE_TAG}:refs/tags/${CIRCLE_TAG}"
+    git fetch ${fetch_tag_args} --depth "${FETCH_DEPTH}" --force --tags origin "+refs/tags/${CIRCLE_TAG}:refs/tags/${CIRCLE_TAG}"
 elif echo $CIRCLE_BRANCH | grep -E ^pull\/[0-9]+/head$ > /dev/null; then
     # pull request called from api. Input should be `pull/123/head` see detail for https://github.com/guitarrapc/git-shallow-clone-orb/issues/34
-    git fetch ${fetch_tag_args} --depth << parameters.fetch_depth >> --force origin "+refs/${CIRCLE_BRANCH}:remotes/origin/${CIRCLE_BRANCH}"
+    git fetch ${fetch_tag_args} --depth "${FETCH_DEPTH}" --force origin "+refs/${CIRCLE_BRANCH}:remotes/origin/${CIRCLE_BRANCH}"
 else
-    git fetch ${fetch_tag_args} --depth << parameters.fetch_depth >> --force origin +refs/heads/${CIRCLE_BRANCH}:refs/remotes/origin/${CIRCLE_BRANCH}
+    git fetch ${fetch_tag_args} --depth "${FETCH_DEPTH}" --force origin +refs/heads/${CIRCLE_BRANCH}:refs/remotes/origin/${CIRCLE_BRANCH}
 fi
 
 # Check the commit ID of the checked out code
